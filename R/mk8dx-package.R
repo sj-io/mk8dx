afe85461198dad50871c2302e8c232f126aa8273
Sarah Johnson <57082282+sj-io@users.noreply.github.com>
2023-04-26 12:24:35 -0500

    created help topic for the package - added ignore_unused_imports - other line was meant to supress a warning   but i don't think it helped

---
+++ R/mk8dx-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+
+ignore_unused_imports <- function() {
+  readr::read_csv()
+}
+
+english <- temp <- NULL
+
