92ebcbd7f84131eb99445fe0ec72b8bc523db461
Sarah Johnson <57082282+sj-io@users.noreply.github.com>
2024-04-07 23:16:17 -0500

    removed mk_track_names & mk_repair* functions - tracks table already exists, mk_repair too messy - still on repair-track-names branch fixed readr unused import namespace

--- R/mk8dx-package.R
+++ R/mk8dx-package.R
@@ -2,12 +2,8 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom readr read_csv
 ## usethis namespace: end
-
-ignore_unused_imports <- function() {
-  readr::read_csv()
-  purrr::map()
-}
 
 utils::globalVariables(c("segment_name_path",
                          "segment_time_path",
